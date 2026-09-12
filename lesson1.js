e1495dcf362c3cfa4801d38f4ef9364db1a6013f
Ahad <mohdahad60080@gmail.com>
2026-09-12 12:51:30 +0530

    new journey

---
+++ lesson1.js
@@ -0,0 +1,37 @@
+console.log("Hello");
+
+console.log("How are you?");
+
+console.log("Goodbye");
+
+let age = 21;
+
+console.log(age);
+
+let score = 100;
+
+const name = "Ahad";
+
+console.log(score);
+
+console.log(name);
+
+score = 200;
+
+console.log(score);
+
+let first_language = "Python";
+
+let second_language = "JavaScript";
+
+console.log(first_language + " " + second_language);
+
+let isAdmin = true;
+
+console.log("Name:", name);
+
+console.log("Age:", age);
+
+age = age + 1;
+
+console.log("Next year:", age);
---
+++ lesson2.js
@@ -0,0 +1,62 @@
+const name = "Ahad";
+const age = 21;
+const numberAge = Number(age);
+const isAdmin = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isAdmin);
+console.log(typeof numberAge);
+
+console.log(typeof undefined);
+
+console.log(typeof null);
+
+const x = 10;
+
+const y = 10.5;
+
+console.log(typeof x);
+console.log(typeof y);
+
+console.log("5" + 2); // string concatenation -> "52"
+console.log("5" - 2); // "5" -> 5  (type coercion) -> 3
+
+let z = 1;
+let zbool = Boolean(z);
+console.log(typeof zbool);
+
+console.log(5 === 5);
+console.log(5 === "5");
+console.log(5 == "5");
+console.log(5 === true);
+
+console.log(10 + 5);
+
+console.log("10" + 5);
+
+console.log("10" - 5);
+
+console.log("10" * 2);
+
+console.log(10 === "10");
+
+console.log(10 == "10");
+
+console.log(typeof 10);
+
+console.log(typeof 10.5);
+
+console.log(typeof "10");
+
+console.log(typeof true);
+
+console.log(typeof null);
+
+console.log(Number("123"));
+
+console.log(String(123));
+
+console.log(Boolean(0));
+
+console.log(Boolean(1));
