576510fb5b23dcdb94b20dbc9a4a92ff0564ba04
Ahad <mohdahad60080@gmail.com>
2026-09-14 13:03:42 +0530

    Logical Operators

--- lesson3.js
+++ lesson3.js
@@ -1,25 +1,33 @@
-const age = 17;
+const age = 13;
+const hasTicket = false;
+const isVIP = false;
 
-if (age < 13){
-	console.log("Child");
-} else if (age < 18){
-	console.log("Teen");
-} else {
-	console.log("Adult");
+if (age >= 18 && hasTicket && isVIP) {
+    console.log("You can enter VIP area");
+} else if (age >= 18 && hasTicket) {
+    console.log("You can enter");
+} else if (age < 18 && hasTicket) {
+    console.log("You can enter play area");
+} else if (age <= 13 && !hasTicket && !isVIP) {
+    console.log("Go home Kid")
+} else if (!hasTicket) {
+    console.log("Get the ticket first");
 }
-
 
 if (true) {
-	console.log("YES");
+    console.log("YES");
 }
 if (1) { // Need a Boolean here, convert this value to true
-	console.log("YES");
+    console.log("YES");
 }
 
-const username = "";
+const username = "Ahad";
+const isAdmin = true;
 
-if (username) {
-	console.log(`Username exists: ${username}`);
+if (username && isAdmin) {
+    console.log(`User is Admin: ${username}`);
+} else if (username) {
+    console.log(`Username exists: ${username}`);
 } else {
-	console.log("User does not exist");
+    console.log("User does not exist.");
 }
