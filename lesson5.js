38ea1f3998c738de146d8818759864a6a0e07638
Ahad <mohdahad60080@gmail.com>
2026-09-15 14:02:54 +0530

    increment & Operator Precedence

--- lesson5.js
+++ lesson5.js
@@ -16,3 +16,20 @@ score *= 10
 
 console.log(score)
 
+// Increment
+let count = 0;
+console.log(count++); // 0 cos: Use the current value, then increment.
+count++; // count = count + 1 or count += 1
+console.log(count)
+
+let subs = 5
+console.log(--subs); // Increment first, then use the value.
+subs--; // subs = subs - 1 or subs -= 1
+console.log(subs)
+
+// Operator Precedence
+console.log(10 + 5 * 2); // 20 because multiplication happens before addition.
+
+console.log((10 + 5) * 2); // 30 > Parentheses override it
+
+
