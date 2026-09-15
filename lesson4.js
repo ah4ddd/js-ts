632df3dbd1bb7370865a527b3574365c6506622c
Ahad <mohdahad60080@gmail.com>
2026-09-15 13:03:59 +0530

    lesson4 done

--- lesson4.js
+++ lesson4.js
@@ -1,4 +1,17 @@
-const userName = "Ahad";
+const userName = "Ahad  ";
 const work = "Coder";
 
-console.log("Hello" + " " + userName + " " + work)
+// Template Literals (`backticks` > insert expression using ${})
+console.log(`Hello ${userName.trim()} ${work}`);
+
+// String Properties
+// console.log(name.length);
+// name.toUpperCase() & console.log(name.toLowerCase());
+
+const message = "I am learning JavaScript";
+// Check whether a string contains something
+console.log(message.includes("JavaScript"));
+
+const language = "JavaScript";
+
+console.log(language.slice(4, 10), language.slice(0, 4));
