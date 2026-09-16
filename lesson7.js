04d3548ee9adeda739e388a99f466b0ebb5a8c03
Ahad <mohdahad60080@gmail.com>
2026-09-16 14:22:52 +0530

    =>

--- lesson7.js
+++ lesson7.js
@@ -39,3 +39,29 @@ console.log(subs(17, 7));
 const result = subs(100, 89);
 console.log(result);
 
+// return Immediately Exits the Function
+function test() {
+    console.log("A");
+    return;
+    console.log("B");
+}
+
+test();
+
+// Functions Can Return Anything
+// A function can encapsulate a piece of logic and give you a value.
+
+// A Function Can Be Used Inside Another Expression
+function mult(a, b) {
+    return a * b;
+}
+
+const multiply = mult(10, 5) / 2;
+
+console.log(multiply)
+
+
+// Arrow Functions
+const div = (a, b) => a / b;
+
+console.log(div(13, 9))
