bf942e89ebb14467c20e4aab0f8eb2848172505c
Ahad <mohdahad60080@gmail.com>
2026-09-16 11:22:33 +0530

    more for loops

--- lesson6.js
+++ lesson6.js
@@ -10,19 +10,27 @@
 
 
 for (let i = 0; i < 6; i++) {
-    console.log(i)
+    console.log(i);
 }
 
-console.log()
-
-for (let i = 5; i > 0; i--) {
-    console.log(i)
-}
-
-console.log()
+console.log();
 
 // Counting Differently
-for (let i = 5; i < 11; i++) {
-    console.log(i)
+for (let i = 5; i <= 10; i++) {
+    console.log(i);
+}
+
+console.log();
+
+//Increment by 2
+for (let i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+console.log();
+
+// Count Backwards
+for (let i = 5; i > 0; i--) {
+    console.log(i);
 }
 
