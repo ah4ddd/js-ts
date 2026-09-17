f04470dad8497515dd08f53ee4cdd9b028e43757
Ahad <mohdahad60080@gmail.com>
2026-09-17 10:40:20 +0530

    arrayyyy

--- lesson8.js
+++ lesson8.js
@@ -1,16 +1,73 @@
 // Arrays
 
-const fruits = ["apple", "banana", "orange"]
+const fruits = ["apple", "banana", "orange"];
 
 // Indexing
-console.log(fruits[0])
-console.log(fruits[2])
-console.log(fruits[1])
+console.log(fruits[0]);
+console.log(fruits[2]);
+console.log(fruits[1]);
 
 // Chaning an Element
-fruits[1] = "mango"
-console.log(fruits[1])
+fruits[1] = "mango";
+console.log(fruits[1]);
 
 // .length
 console.log(fruits.length);
-console.log(fruits[fruits.length - 1])
+console.log(fruits[fruits.length - 1]);
+
+// Adding Elements
+fruits.push("kiwi");
+fruits.push("fig")
+console.log(fruits[3]);
+
+// Removing From the End
+console.log(fruits);
+fruits.pop();
+console.log(fruits);
+
+// looping
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+console.log();
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
+
+// Add to Beginning
+console.log(fruits);
+fruits.unshift("cherry");
+console.log(fruits);
+
+// Remove From Beginning
+fruits.shift();
+console.log(fruits);
+
+/*
+push()      → add end
+pop()       → remove end
+
+unshift()   → add beginning
+shift()     → remove beginning
+*/
+
+// Arrays Can Contain Different Types
+const data = [
+    "Ahad",
+    21,
+    true,
+    null
+];
+
+// Arrays Can Contain Arrays
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(matrix[0][0]);
+console.log(matrix[2][2]);
+
+
