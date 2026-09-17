79b47e1c36bc762f9b524e54f43a50bf2fb7c19e
Ahad <mohdahad60080@gmail.com>
2026-09-17 10:18:04 +0530

    L8S

---
+++ lesson8.js
@@ -0,0 +1,16 @@
+// Arrays
+
+const fruits = ["apple", "banana", "orange"]
+
+// Indexing
+console.log(fruits[0])
+console.log(fruits[2])
+console.log(fruits[1])
+
+// Chaning an Element
+fruits[1] = "mango"
+console.log(fruits[1])
+
+// .length
+console.log(fruits.length);
+console.log(fruits[fruits.length - 1])
