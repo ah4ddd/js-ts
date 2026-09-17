9042238db07d9681a3b4c02bcd6cea85e2c85223
Ahad <mohdahad60080@gmail.com>
2026-09-17 14:24:04 +0530

    bread&butter

--- lesson9.js
+++ lesson9.js
@@ -61,23 +61,18 @@ const team = [
 ];
 console.log(team[0].name);
 
-// Methods
+// Methods -> Function stored object property is commonly called method
 user.greet = function (user) {
     console.log(`Hello ${user}!`);
 }
 user.greet(user.name);
 
-//
-
-// Take the value currently stored at freq[num].
-//  If it doesn't exist (or is falsy), use 0. Add 1 to that.
-// Then assign the result back into freq[num]
-const nums = [2, 2, 3];
-
-const freq = {};
-
-for (const num of nums) {
-    freq[num] = (freq[num] || 0) + 1;
+// A Method Can Access the Object
+// this.name refers to the name associated with the object in this method call\
+// Objects can have methods, and methods can work with object data.
+user.dream = function () {
+    console.log(`${this.name} wants to be free`);
 }
+user.dream();
 
-console.log(freq);
+// Objects Are Going to Become Your Backend Bread and Butter
