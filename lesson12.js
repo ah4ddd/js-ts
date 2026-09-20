db1f4241d3ad4edea271ae5425bf26d32efd86f6
Ahad <mohdahad60080@gmail.com>
2026-09-20 12:49:22 +0530

    Callback function

--- lesson12.js
+++ lesson12.js
@@ -1,1 +1,24 @@
 // Array Methods
+// A method is a function associated with an object.
+// Since arrays are objects in JavaScript, arrays have their own methods
+
+const numbers = [10, 20, 30];
+numbers.push(40);
+/*  │     │   │
+    │     │   └── argument
+    │     └─────── method
+    └────────────── array/object
+*/
+
+// Callback function
+// A function that you give to another function/method
+// so that it can call your function later.
+function greet() {
+    console.log("Hello");
+}
+
+function doit(cb) {
+    cb();
+}
+
+doit(greet);
