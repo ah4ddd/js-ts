0fdd1a67ac9b43c155e318d4f5399f380045521b
Ahad <mohdahad60080@gmail.com>
2026-09-23 13:15:22 +0530

    global scope

--- lesson14.js
+++ lesson14.js
@@ -1,1 +1,28 @@
 // Scope
+// The region of your code where a variable is accessible.
+// From this exact location in the code, can JavaScript see this variable?
+
+// Global Scope
+const country = "India";
+// Inner scopes can access variables from outer scopes.
+function showCountry() {
+    if (country) {
+        console.log(country);
+    }
+}
+
+showCountry();
+
+// Outer → Inner does NOT work
+// Function Scope
+// Every function creates its own scope.
+
+function test() {
+    const x = 10;
+    console.log(x ** x);
+}
+
+test();
+// console.log(x); <- Fails cos x belongs to test();
+
+// BLOCK SCOPE
