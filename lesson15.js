2c0b5d24bf125c27ad78c9b6f58539020bf8a4ab
Ahad <mohdahad60080@gmail.com>
2026-09-24 13:19:44 +0530

    Closures + scope chain

--- lesson15.js
+++ lesson15.js
@@ -29,6 +29,11 @@ fn();
 // Returning a function is just a very obvious way to demonstrate a closure.
 // But production, returning the inner function is absolutely
 // a normal pattern when a closure is what you need.
+
+// Create the counter once → it creates count = 0 and
+// increment → increment is returned and stored in counter
+// → every counter() call runs increment,
+// which modifies the same remembered count.
 function createCounter() {
     let count = 0;
 
@@ -46,6 +51,20 @@ counter(); // 1
 counter(); // 2
 counter(); // 3
 
+// Closures + scope chain
+// Closure doesn't destroy that rule.
+// Instead, the inner function retains access to its lexical scope.
+const global = "GLOBAL";
+function gOuter() {
+    const outerValue = "OUTER";
+    return function inner() {
+        console.log(global);
+        console.log(outerValue);
+    };
+}
+
+const gs = gOuter();
+gs();
 
 // errorLog is a function that remembers prefix.
 function createLogger(prefix) {
