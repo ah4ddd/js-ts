f1253a0c16e2cb9ffef72326ff1f0214a6f6b0f5
Ahad <mohdahad60080@gmail.com>
2026-09-25 18:42:03 +0530

    fix: standardize transaction types and descriptions in user data

--- projects/personalFinanceExpenseManager.js
+++ projects/personalFinanceExpenseManager.js
@@ -6,21 +6,21 @@ const user = {
     transactions: [
         {
             id: 1,
-            type: "Income",
+            type: "income",
             amount: 50000,
             category: "Salary",
             description: "Monthtly salary"
         },
         {
             id: 1,
-            type: "Income",
+            type: "income",
             amount: 10000,
             category: "Freelance",
             description: "Gig work"
         },
         {
             id: 1,
-            type: "saving",
+            type: "expense",
             amount: 5000,
             category: "Investing",
             description: "SIP"
@@ -38,7 +38,40 @@ const user = {
             amount: 2000,
             category: "Eating out",
             descriiption: "Just living"
+        },
+        {
+            id: 1,
+            type: "expense",
+            amount: 1500,
+            category: "Transport",
+            descriiption: "here and there"
+        },
+        {
+            id: 1,
+            type: "expense",
+            amount: 3500,
+            category: "Bills",
+            descriiption: "Gas, Internet, electricity, etc"
         }
 
     ]
+};
+
+const selectUserId = 1;
+const users = [user];
+
+const selectedUser = users.find(
+    user => user.id === selectUserId
+);
+
+function totalIncome() {
+    let total = 0;
+    for (const transaction of selectedUser.transactions) {
+        if (transaction.type === "income") {
+            total += transaction.amount;
+        }
+    }
+    return total;
 }
+
+console.log(`Total Income: ${totalIncome()}`);
