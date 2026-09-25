539c3b1b9b4517fc40724b6da9a46a8856e87d8a
Ahad <mohdahad60080@gmail.com>
2026-09-25 18:24:24 +0530

    start p2

---
+++ projects/personalFinanceExpenseManager.js
@@ -0,0 +1,44 @@
+const user = {
+    id: 1,
+    userName: "Ahad",
+    age: 21,
+
+    transactions: [
+        {
+            id: 1,
+            type: "Income",
+            amount: 50000,
+            category: "Salary",
+            description: "Monthtly salary"
+        },
+        {
+            id: 1,
+            type: "Income",
+            amount: 10000,
+            category: "Freelance",
+            description: "Gig work"
+        },
+        {
+            id: 1,
+            type: "saving",
+            amount: 5000,
+            category: "Investing",
+            description: "SIP"
+        },
+        {
+            id: 1,
+            type: "expense",
+            amount: 8000,
+            category: "Groceries",
+            description: "Just survival"
+        },
+        {
+            id: 1,
+            type: "expense",
+            amount: 2000,
+            category: "Eating out",
+            descriiption: "Just living"
+        }
+
+    ]
+}
